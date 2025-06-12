981a0a0cc07a18dc016e4b681ead84cfc822d5c0
jozorio29 <joseozorio29@gmail.com>
2025-06-12 12:21:51 -0300

    remove deprecated useUnifiedTopology option

--- server/config/music.config.js
+++ server/config/music.config.js
@@ -3,20 +3,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-
 const URI_MONGODB = process.env.MONGODB_URI;
 
 const conectarDB = async () => {
-    try {
-        await mongoose.connect(URI_MONGODB, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log("Base de datos conectada");
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
-};  
+  try {
+    await mongoose.connect(URI_MONGODB, {
+      useNewUrlParser: true,
+    });
+    console.log("Base de datos conectada");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
-export default conectarDB
+export default conectarDB;
